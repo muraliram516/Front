394cf82db119672e239e4209c424b5687be99247
muraliram516 <115497046+muraliram516@users.noreply.github.com>
2024-01-15 18:11:34 +0530

    Add files via upload

---
+++ Seller Dashboard updated/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Product from './Products';
+const App = () => {
+  return (
+    <div className='text-center'>
+      <h1>Seller Admin Dashboard</h1>
+      <Product />
+    </div>
+  );
+};
+
+export default App;
