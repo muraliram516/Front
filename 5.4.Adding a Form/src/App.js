d6923a5413b995afd093128f24bb99262b3c2514
muraliram516 <115497046+muraliram516@users.noreply.github.com>
2024-01-16 12:18:07 +0530

    Add files via upload

---
+++ 5.4.Adding a Form/src/App.js
@@ -0,0 +1,16 @@
+import {Fragment} from 'react';
+import Header from './Layout/Header';
+import Meals from './Meals/Meals';
+
+function App() {
+  return (
+    <Fragment>
+    <Header />
+      <main >
+    <Meals />
+        </main>
+    </Fragment>
+  );
+}
+
+export default App;
